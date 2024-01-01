04d0d06194f8766785e25d74015a398c978f5a69
Marcin Hodor <mhodor@o2.pl>
2024-01-01 22:25:28 +0100

    add cookie-parser, jest and supertest packages

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from "express"
 import registerRoute from "./routes/api/register"
+import cookieParser from "cookie-parser"
+
 const PORT = process.env.PORT || 3500
 
 const app = express()
+
+// json and cookie parser middlewares
+app.use(express.json())
+app.use(cookieParser())
 
 //routes
 app.use("/register", registerRoute)
