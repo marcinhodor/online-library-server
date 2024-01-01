d198b4c9790ee6957434f56d43297c671a9cd7f6
Marcin Hodor <mhodor@o2.pl>
2024-01-01 23:42:51 +0100

    add login handler

--- src/auth/Auth.service.ts
+++ src/auth/Auth.service.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { LoginDto, RegisterDto } from "./dto/auth.dto"
 
 const prisma = new PrismaClient()
 
 export const register = async (req: Request, res: Response) => {
   // get data from request body
-  const { first_name, last_name, email, password } = req.body
+  const { first_name, last_name, email, password } = req.body as RegisterDto
   // validate if body data is complete
   if (!email || !password || !first_name || !last_name) {
     return res.status(400).json({
@@ -43,4 +45,49 @@ export const register = async (req: Request, res: Response) => {
       error: "error.message",
     })
   }
+}
+
+export const login = async (req: Request, res: Response) => {
+  // get data from request body
+  const { email, password } = req.body as LoginDto
+  // validate if body data is complete
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Body must contain email and password",
+    })
+  }
+  // check if user exists
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+  // if user does not exist, return error
+  if (!foundUser) return res.sendStatus(401) // unauthorized
+  // check if password is correct
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    foundUser.hashed_password
+  )
+  // if password is incorrect, return error
+  if (!isPasswordCorrect) return res.sendStatus(401) // unauthorized
+
+  // if user exists and password is correct, create jwt token
+  const token = jwt.sign(
+    {
+      id: foundUser.id,
+      email: foundUser.email,
+    },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "3h" }
+  )
+
+  // return token in a cookie
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 3 * 60 * 60 * 1000,
+  })
+  res.status(200).json({ success: `User logged in successfully` })
 }
