49b287657671583ffb879ef0a81ebba92fe58564
Marcin Hodor <mhodor@o2.pl>
2024-01-07 20:58:02 +0100

    minor change

--- src/auth/Auth.controller.ts
+++ src/auth/Auth.controller.ts
@@ -2,6 +2,7 @@ import express from "express"
 import { login, logout, register } from "../auth/auth.service"
 
 const router = express.Router()
+
 router.post("/register", register)
 router.post("/login", login)
 router.get("/logout", logout)
